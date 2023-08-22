33ac3f0c3771e7ae9552122d5b8fe5e8922cfc77
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2023-08-23 01:32:17 +0800

    t.cpp

--- sieve.cpp
+++ sieve.cpp
@@ -1,11 +1,18 @@
 vector<int> factors(int n) {
+    std::vector<bool> dp(n + 1, true);
+
+    for (int p = 2; p * p <= n; ++p) {
+        if (dp[p]) {
+            for (int i = p * p; i <= n; i += p) {
+                dp[i] = false;
+            }
+        }
+    }
     vector<int> f;
-    for (int x = 2; x * x <= n; x++) {
-    while (n % x == 0) {
-        f.push_back(x);
-        n/= x;
+    for (int p = 2; p <= n; ++p) {
+        if (dp[p]) {
+            f.push_back(p);
+        }
     }
-    }
-    if (n > 1) f.push_back(n);
     return f;
 }
